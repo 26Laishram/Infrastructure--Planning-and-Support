8e8377dd10539da08bb31cc1eb6957660b1b882d
Laishram Munglemkhombi Devi <munglemkhombi123@gmail.com>
2025-10-30 10:54:44 +0530

    projected uri

--- iit-infra-backend/connect.js
+++ iit-infra-backend/connect.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
-const uri = "mongodb+srv://ipswebsite:ipswebsite@cluster0.4yx435c.mongodb.net/";
+const uri = process.env.MONGODB_URI;
 
 mongoose.connect(uri)
   .then(() => console.log("✅ Connected to MongoDB Atlas"))
